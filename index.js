36093653797daf4a4ff6828f78e1b801f3aec5f1
giomamiz <gio@ot3.co>
2017-11-27 20:10:13 -0500

    handles post now

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ let mongo = require('mongodb'),
 	],
 	store = {},
 	http = require('http'),
-	hostname = '0.0.0.0',
-	port = 80,
+	hostname = '127.0.0.1',
+	port = 3001,
 	server = null;
 
 
@@ -37,7 +37,7 @@ function webserver() {
 						}
 						for(let i = 0; i < properties.length; i++){
 							if(properties[i].length > 0 && properties[i].split('>').length > 1){
-								assembled[properties[i].split('>')[0]] = properties[i].split('>')[1];
+								assembled[properties[i].split('>')[0].trim()] = properties[i].split('>')[1];
 								continue;
 							}
 						}
@@ -45,105 +45,129 @@ function webserver() {
 						return assembled;
 					},
 					assembledQuery = assemble(query),
-					assembledWriteData = assemble(writeData);
+					assembledWriteData = assemble(writeData),
+					handle = () => {
+						switch(operation) {
+							case 'create':
+								if(!assembledQuery) {
+									db.createCollection(collection, (err, results) => {
+										if(err){
+											res.statusCode = 401;
+											res.end(JSON.stringify({
+												status: 'error',
+												result: 'Creating collection threw error'
+											}));
+										}
+										res.statusCode = 200;
+										res.end(JSON.stringify({
+											status: 'success',
+											result: 'Collection '+collection+' created'
+										}));
+									});
+									break;
+								}
+								db.collection(collection).insertOne(JSON.parse(assembledQuery), (err, result) => {
+									res.statusCode = 200;
+									res.end(JSON.stringify({
+										status: 'success',
+										result: result
+									}));
+								});
+								break;
+							case 'read':
+								// let _query = 
+								db.collection(collection).find(JSON.parse(assembledQuery)).toArray((err, result) => {
+									if(result){
+										res.statusCode = 200;
+										res.end(JSON.stringify({
+											status: 'success',
+											result: result
+										}));
+									}
+								});
+								break;
+							case 'delete':
+								if(!query || !assembledQuery){
+									res.statusCode = 401;
+									res.end(JSON.stringify({
+										status:'error',
+										result: 'Deletion of an entire collection is currently locked'
+									}))
+								}
+								db.collection(collection).deleteOne(JSON.parse(assembledQuery), (err, result) => {
+									if(err) {
+										res.statusCode = 401;
+										res.end(JSON.stringify({
+											status: 'error',
+											result: 'Deletion of '+query+' in collection '+collection+' returned an error'
+										}))
+									}
+									res.statusCode = 200;
+									res.end(JSON.stringify({
+										status: 'success',
+										result: 'Deletion of '+query+' in collection '+collection+' success'
+									}));
+								})
+								break;
+							case 'update':
+								let update = (mergedWriteData) => {
+									db.collection(collection).updateOne(JSON.parse(assembledQuery), mergedWriteData, (err, result) => {
+										if(err) {
+											res.statusCode = 401;
+											res.end(JSON.stringify({
+												status: 'error',
+												result: 'Could not update '+query+' in collection '+collection
+											}))
+										}
+										res.statusCode = 200;
+										res.end(JSON.stringify({
+											status: 'success',
+											result: 'Updated '+query+' in collection '+collection
+										}))
+									});
+								};
+								db.collection(collection).findOne(JSON.parse(assembledQuery), (err, result) => {
+									if(err) {
+										res.statusCode = 401;
+										res.end(JSON.stringify({
+											status:'error',
+											result: 'Could not find record '+query+' to update in collection '+collection
+										}));
+									}
+									update(Object.assign({}, result, JSON.parse(assembledWriteData)));
+								});
+								break;
+							};
+					}
 
 			res.setHeader('Content-Type', 'application/json');
 			res.setHeader("Access-Control-Allow-Origin", "*");
 
-			switch(operation) {
-				case 'create':
-					if(!query) {
-						db.createCollection(collection, (err, results) => {
-							if(err){
-								res.statusCode = 401;
-								res.end(JSON.stringify({
-									status: 'error',
-									result: 'Creating collection threw error'
-								}));
-							}
-							res.statusCode = 200;
-							res.end(JSON.stringify({
-								status: 'success',
-								result: 'Collection '+collection+' created'
-							}));
-						});
-						break;
-					}
-					db.collection(collection).insertOne(assembledQuery, (err, result) => {
-						res.statusCode = 200;
-						res.end(JSON.stringify({
-							status: 'success',
-							result: result
-						}));
-					});
-					break;
-				case 'read':
-					// let _query = 
-					db.collection(collection).find(assembledQuery).toArray((err, result) => {
-						if(result){
-							res.statusCode = 200;
-							res.end(JSON.stringify({
-								status: 'success',
-								result: result
-							}));
-						}
-					});
-					break;
-				case 'delete':
-					if(!query || !assembledQuery){
-						res.statusCode = 401;
-						res.end(JSON.stringify({
-							status:'error',
-							result: 'Deletion of an entire collection is currently locked'
-						}))
-					}
-					db.collection(collection).deleteOne(assembledQuery, (err, result) => {
-						if(err) {
-							res.statusCode = 401;
-							res.end(JSON.stringify({
-								status: 'error',
-								result: 'Deletion of '+query+' in collection '+collection+' returned an error'
-							}))
-						}
-						res.statusCode = 200;
-						res.end(JSON.stringify({
-							status: 'success',
-							result: 'Deletion of '+query+' in collection '+collection+' success'
-						}));
-					})
-					break;
-			case 'update':
-				let update = (mergedWriteData) => {
-					db.collection(collection).updateOne(assembledQuery, mergedWriteData, (err, result) => {
-						if(err) {
-							res.statusCode = 401;
-							res.end(JSON.stringify({
-								status: 'error',
-								result: 'Could not update '+query+' in collection '+collection
-							}))
-						}
-						res.statusCode = 200;
-						res.end(JSON.stringify({
-							status: 'success',
-							result: 'Updated '+query+' in collection '+collection
-						}))
-					});
-				};
-				db.collection(collection).findOne(assembledQuery, (err, result) => {
-					if(err) {
-						res.statusCode = 401;
-						res.end(JSON.stringify({
-							status:'error',
-							result: 'Could not find record '+query+' to update in collection '+collection
-						}));
-					}
-					update(Object.assign({}, result, assembledWriteData));
-				});
-				break;
-			};
+			console.log(req.body)
+
+			if (req.method == 'POST') {
+        req.on('data', (data) => {
+        	if(!query) {
+        		assembledQuery = assembledQuery || '';
+        		assembledQuery += data;
+        	} else {
+        		assembledWriteData = assembledWriteData || '';
+            assembledWriteData += data;
+        	}
+        });
+        req.on('end', () => {
+        	assembledQuery && JSON.parse(assembledQuery);
+          assembledWriteData && JSON.parse(assembledWriteData);
+          handle();
+        });
+        return;
+			}
+			handle();
 			return;
-		});
+	});
 }
+
+
 
 function init() {
 	database((_db)=>{
